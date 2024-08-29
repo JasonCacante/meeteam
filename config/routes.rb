dfecb099015d021adb63fc74ba134f7cdcc4811b
JasonCacanteAgri <jcacante@agricapital.com.co>
2024-08-29 18:43:49 -0500

    fix(routes.rb): change url route to /products

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  post '/', to: 'products#index'
+  post '/products', to: 'products#index'
 end
