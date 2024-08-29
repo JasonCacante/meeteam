b4e72697552508d8cd70fded23a8ebd0176c31cf
JasonCacanteAgri <jcacante@agricapital.com.co>
2024-08-29 18:01:06 -0500

    feat(products_controller.rb): add products index endpoint with validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,24 @@
 class ProductsController < ApplicationController
+  before_action :pagiante_params, only: %i[index]
+
   def index
-    @products = Product.paginate(page: params[:page], per_page: 10)
-    render json: @products
+    @products = Product.paginate(
+      page: params[:page],
+      per_page: params[:per_page],
+      key: params[:key],
+      sort_by: params[:sort_by],
+      sort_order: params[:sort_order]
+    )
+    render json: @products, status: :ok
+  end
+
+  private
+
+  # Define strong parameters for products
+  def pagiante_params
+    params.require(%i[page per_page])
+    params.permit(%i[page per_page key sort_by sort_order])
+
+    render json: { message: 'Page starts at 1' }, status: :bad_request if params[:page].to_i < 1
   end
 end
